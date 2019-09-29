c9ab3bfa1119c33f05117bdf650039862fbc658e
wigutierrez5 <wigutierrez5@poligran.edu.co>
2019-09-29 03:28:53 -0500

    arreglando conflictos

--- ProyectoFronStarWars/AppStarWars/e2e/src/app.e2e-spec.ts
+++ ProyectoFronStarWars/AppStarWars/e2e/src/app.e2e-spec.ts
@@ -1,8 +1,4 @@
 import { AppPage } from './app.po';
-<<<<<<< HEAD
-import { browser, logging } from 'protractor';
-=======
->>>>>>> eda25b8ea7734e4ddcefe4850db1b66293b8437f
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -13,18 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-<<<<<<< HEAD
-    expect(page.getTitleText()).toEqual('AppStarWars app is running!');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-=======
     expect(page.getTitleText()).toEqual('Welcome to AppStarWars!');
->>>>>>> eda25b8ea7734e4ddcefe4850db1b66293b8437f
   });
 });
