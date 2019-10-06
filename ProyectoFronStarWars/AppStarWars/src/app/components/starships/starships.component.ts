b7676f1f63faf00df6acb693a22ca37c83ec1a64
andresfqm2 <anquintana2@poligran.edu.co>
2019-10-05 23:54:49 -0500

    se completan las vistas del navbar

--- ProyectoFronStarWars/AppStarWars/src/app/components/starships/starships.component.ts
+++ ProyectoFronStarWars/AppStarWars/src/app/components/starships/starships.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataApiService } from 'src/app/services/data-api.service';
+import { starshipsInterface } from 'src/app/models/starships-interfaces';
 
 @Component({
   selector: 'app-starships',
@@ -10,25 +11,32 @@ export class StarshipsComponent implements OnInit {
 
   constructor(private dataApi: DataApiService) { }
 
-  private starShips: Starships ={
+  private starShips: starshipsInterface ={
     name: '',
-    height: '',
-    mass: '',
-    hair_color: '',
-    skin_color: '',
-    eye_color: '',
-    birth_year: '', 
-    gender: ''
+    model: '',
+    manufacturer: '',
+    cost_in_credits: '',
+    length: '',
+    max_atmosphering_speed: '',
+    crew: '',
+    passengers: '',
+    cargo_capacity: '',
+    consumables: '',
+    hyperdrive_rating: '',
+    MGLT: '',
+    starship_class: '',
+    pilots: '',
+    films: ''
     
   }
 
   ngOnInit() {
-    this.getListPeoples();
+    this.getListStarShips();
 
   }
 
-  getListPeoples(){
-    this.dataApi.getAllPeople().subscribe((people: PeopleInterface) =>(this.people = people));
+  getListStarShips(){
+    this.dataApi.getAllStarships().subscribe((starShips: starshipsInterface) =>(this.starShips = starShips));
   }
 
 
