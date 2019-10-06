f596101ea00f79770ced748f0b154fa5d4c3e8d9
andresfqm2 <anquintana2@poligran.edu.co>
2019-10-05 23:07:47 -0500

    Se agregan las vistas del navbar

--- ProyectoFronStarWars/AppStarWars/src/app/components/starships/starships.component.ts
+++ ProyectoFronStarWars/AppStarWars/src/app/components/starships/starships.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/services/data-api.service';
 
 @Component({
   selector: 'app-starships',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApi: DataApiService) { }
+
+  private starShips: Starships ={
+    name: '',
+    height: '',
+    mass: '',
+    hair_color: '',
+    skin_color: '',
+    eye_color: '',
+    birth_year: '', 
+    gender: ''
+    
+  }
 
   ngOnInit() {
+    this.getListPeoples();
+
   }
+
+  getListPeoples(){
+    this.dataApi.getAllPeople().subscribe((people: PeopleInterface) =>(this.people = people));
+  }
+
 
 }
